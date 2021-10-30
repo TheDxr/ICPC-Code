e78fb9fba1036929f21cb8751fb568f240d6a790
TheDxr <872151901@qq.com>
2021-10-30 16:44:44 +0800

    -

---
+++ LeetCode/面试题 16.25. LRU 缓存.cpp
@@ -0,0 +1,35 @@
+class LRUCache {
+public:
+    LRUCache(int capacity) {
+
+    }
+    
+    int get(int key) {
+
+    }
+    
+    void put(int key, int value) {
+
+    }
+};
+// LRUCache cache = new LRUCache( 2 /* 缓存容量 */ );
+
+// cache.put(1, 1);
+// cache.put(2, 2);
+// cache.get(1);       // 返回  1
+// cache.put(3, 3);    // 该操作会使得密钥 2 作废
+// cache.get(2);       // 返回 -1 (未找到)
+// cache.put(4, 4);    // 该操作会使得密钥 1 作废
+// cache.get(1);       // 返回 -1 (未找到)
+// cache.get(3);       // 返回  3
+// cache.get(4);       // 返回  4
+
+// 来源：力扣（LeetCode）
+// 链接：https://leetcode-cn.com/problems/lru-cache-lcci
+// 著作权归领扣网络所有。商业转载请联系官方授权，非商业转载请注明出处。
+/**
+ * Your LRUCache object will be instantiated and called as such:
+ * LRUCache* obj = new LRUCache(capacity);
+ * int param_1 = obj->get(key);
+ * obj->put(key,value);
+ */
--- main.cpp
+++
@@ -1,9 +0,0 @@
-#include <bits/stdc++.h>
-using namespace std;
-
-int main()
-{
-    std::ios::sync_with_stdio(false);
-    cin.tie(NULL);
-    cout << "test1" << endl;
-}
